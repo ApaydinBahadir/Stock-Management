f710218adb0ed578c5edd1f24b6683bf65bff2b2
ApaydinBahadir <80626707+ApaydinBahadir@users.noreply.github.com>
2022-03-29 22:36:47 +0300

    upload files

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation;
+using Northwind.Business.Abstract;
+using Northwind.Business.ValidationRules.FluentValidation;
+using Northwind.DataAccess.Abstract;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            ProductValidator productValidator = new ProductValidator();
+            var result = productValidator.Validate(product);
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetProductsByCategory(int categoryID)
+        {
+            return _productDal.GetAll(P => P.CategoryID == categoryID);
+        }
+
+        public List<Product> GetProductsByProduct(string productName)
+        {
+            return _productDal.GetAll(p => p.ProductName.ToLower().Contains(productName.ToLower()));
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
